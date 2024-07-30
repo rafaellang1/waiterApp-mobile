104b36e53bd9c51652ca7f2cb9a34cce9080a2c1
rafaellang1 <rafael.lang9@gmail.com>
2024-07-29 23:53:24 -0400

    Add: category component

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -3,10 +3,20 @@ import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 
 import { Category, Icon } from './styles';
+import { useState } from 'react';
 
 
 // renderizar componentes em lista com Flatlist
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+
+    setSelectedCategory(category);
+  }
+
   return (
     <FlatList
       horizontal
@@ -15,15 +25,23 @@ export function Categories() {
       contentContainerStyle={{ paddingRight: 24 }}
       data={categories}
       keyExtractor={category => category._id}
-      renderItem={({ item: category }) => (
-        <Category>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
 
-          <Text size={14} weight="600">{category.name}</Text>
-        </Category>
-      )}
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>
+                {category.icon}
+              </Text>
+            </Icon>
+
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
+              {category.name}
+            </Text>
+          </Category>
+        );
+      }}
     />
   );
 }
